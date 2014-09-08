7130227d5aa033d4db0cc29b6d6e3ed77c7d0086
jrcawthon42 <jrcawthon42@students.tntech.edu>
2014-09-08 12:06:23 -0500

    change readfile.h from struct to class

--- ReadFile.cpp
+++ ReadFile.cpp
@@ -4,43 +4,43 @@
 
 ReadFile* createReadFile(const char* file_name)
 {
-   ReadFile* rf = new ReadFile;
+	ReadFile* rf = new ReadFile;
 
-   rf->input_file.open(file_name);
-   rf->closed = false;
-   rf->_eof = false;
+	rf->input_file.open(file_name);
+	rf->closed = false;
+	rf->_eof = false;
 
-   return rf;
+	return rf;
 }
 
 void destroyReadFile(ReadFile* rf)
 {
-   close(rf);
-   delete rf;
+	close(rf);
+	delete rf;
 }
 
 bool eof(ReadFile* rf)
 {
-   return rf->_eof;
+	return rf->_eof;
 }
 
 void close(ReadFile* rf)
 {
-   if (!rf->closed)
-   {
-      rf->input_file.close();
-      rf->closed = true;
-   }
+	if (!rf->closed)
+	{
+		rf->input_file.close();
+		rf->closed = true;
+	}
 }
 
 String* readLine(ReadFile* rf)
 {
-   if (rf->closed) return NULL;
-   if (rf->_eof) return NULL;
+	if (rf->closed) return NULL;
+	if (rf->_eof) return NULL;
 
-   string text;
-   rf->_eof = !(getline(rf->input_file, text));
+	string text;
+	rf->_eof = !(getline(rf->input_file, text));
 
-   String* str = new String((const char*) text.c_str());
-   return str;
+	String* str = new String((const char*)text.c_str());
+	return str;
 }
--- ReadFile.h
+++ ReadFile.h
@@ -4,19 +4,59 @@
 #include "Text.h"
 
 #include <fstream>
+#include <iostream>
 using namespace std;
+class ReadFile {
 
-struct ReadFile
-{
-   ifstream input_file;
-   bool _eof;
-   bool closed;
+private:
+	bool _eof;
+	ifstream input_file;
+	bool closed;
+
+public:
+	//ReadFile* ReadFile::createReadFile(const char* file_name);
+	ReadFile(const char* file_name);
+	virtual ~ReadFile();
+	String* readLine();
+	bool eof();
+	void close();
+	/*
+	void ReadFile::destroyReadFile(ReadFile* rf)
+	{
+		close(rf);
+		delete[] rf;
+	};
+
+	String* readLine(ReadFile* rf)
+	{
+		
+
+		if (rf->closed) return NULL;
+		if (rf->_eof) return NULL;
+
+		string text;
+		rf->_eof = !(getline(rf->input_file, text));
+
+		String* str = new String((const char*)text.c_str());
+		return str;
+	};
+
+	bool eof(ReadFile* rf)
+	{
+		
+
+		return rf->_eof;
+	};
+
+	void close(ReadFile* rf)
+	{
+		
+
+		if (!rf->closed)
+		{
+			rf->input_file.close();
+			rf->closed = true;
+		}
+	};*/
 };
-
-ReadFile* createReadFile(const char* file_name);
-void destroyReadFile(ReadFile* rf);
-String* readLine(ReadFile* rf);
-bool eof(ReadFile* rf);
-void close(ReadFile* rf);
-
 #endif
