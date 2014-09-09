7d2d0fe770e6be739936f84de7d91f384c2980f2
jrcawthon42 <jrcawthon42@students.tntech.edu>
2014-09-09 13:46:26 -0500

    change writefile.h from struct to class

--- WriteFile.h
+++ WriteFile.h
@@ -6,15 +6,17 @@
 #include <fstream>
 using namespace std;
 
-struct WriteFile
+class WriteFile
 {
-   ofstream output_file;
-   bool closed;
+	private:
+		ofstream output_file;
+		bool closed;
+
+	public:
+		WriteFile(const char* file_name);
+		virtual ~WriteFile();
+		void writeLine();
+		void close();
+
 };
-
-WriteFile* createWriteFile(const char* file_name);
-void destroyWriteFile(WriteFile* wf);
-void writeLine(WriteFile* wf, String* line);
-void close(WriteFile* wf);
-
 #endif
